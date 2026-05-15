6e5370e6e9eb3110c49384ce29f909b5dd07855a
Anton Trafimovich <tonytrof@gmail.com>
2026-05-15 13:12:20 +0200

    In progress with proxy.

---
+++ http-proxy/basic/proxy.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/ip.h>
+#include <arpa/inet.h>
+#include <string.h>
+
+int request_is_complete(char *buf)
+{
+    return strstr(buf, "\r\n\r\n") != NULL;
+}
+
+int main(int argc, char *argv[])
+{
+    int sfd;
+
+    if ((sfd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+    {
+        perror("socket failed");
+        return 1;
+    }
+
+    int opt = 1;
+    setsockopt(sfd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+
+    struct in_addr in_addr;
+
+    if (inet_pton(AF_INET, "0.0.0.0", &in_addr) < 1)
+    {
+        perror("inet_pton failed");
+        return 1;
+    }
+
+    struct sockaddr_in addr =
+        {
+            AF_INET,
+            htons(8082),
+            in_addr};
+
+    if (bind(sfd, (struct sockaddr *)&addr, sizeof(addr)) < 0)
+    {
+        perror("bind failed");
+        return 1;
+    }
+
+    if (listen(sfd, 1024) != 0)
+    {
+        perror("listen failed");
+        return 1;
+    }
+
+    while (1)
+    {
+        struct sockaddr_in addrc = {0};
+        socklen_t addrclen = 0;
+        int cfd;
+        if ((cfd = accept(sfd, (struct sockaddr *)&addrc, &addrclen)) == -1)
+        {
+            continue;
+        }
+
+        uint8_t buf[1024] = {0};
+        ssize_t used = 0;
+        while (1)
+        {
+            ssize_t recvr = recv(cfd, buf + used, 1024 - used, 0);
+            used += recvr;
+
+            if (recvr == -1)
+            {
+                continue;
+            }
+
+            if (recvr == 0 || request_is_complete(buf))
+            {
+                break;
+            }
+
+            printf("%s\n", (char *)buf);
+        }
+
+        printf("Data has been received\n");
+    }
+}
