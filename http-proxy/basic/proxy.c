7bb47a7c1d05a74f39d0b97c5aa694b5440e36e9
Anton Trafimovich <tonytrof@gmail.com>
2026-05-18 14:35:47 +0200

    Basic proxy is working.

--- http-proxy/basic/proxy.c
+++ http-proxy/basic/proxy.c
@@ -10,7 +10,7 @@ int request_is_complete(char *buf)
     return strstr(buf, "\r\n\r\n") != NULL;
 }
 
-int main(int argc, char *argv[])
+int tcp_listen(const char *host, in_port_t port)
 {
     int sfd;
 
@@ -25,7 +25,7 @@ int main(int argc, char *argv[])
 
     struct in_addr in_addr;
 
-    if (inet_pton(AF_INET, "0.0.0.0", &in_addr) < 1)
+    if (inet_pton(AF_INET, host, &in_addr) < 1)
     {
         perror("inet_pton failed");
         return 1;
@@ -34,7 +34,7 @@ int main(int argc, char *argv[])
     struct sockaddr_in addr =
         {
             AF_INET,
-            htons(8082),
+            htons(port),
             in_addr};
 
     if (bind(sfd, (struct sockaddr *)&addr, sizeof(addr)) < 0)
@@ -49,6 +49,13 @@ int main(int argc, char *argv[])
         return 1;
     }
 
+    return sfd;
+}
+
+int main(int argc, char *argv[])
+{
+    int sfd = tcp_listen("0.0.0.0", 8082);
+
     while (1)
     {
         struct sockaddr_in addrc = {0};
@@ -59,12 +66,32 @@ int main(int argc, char *argv[])
             continue;
         }
 
+        int scon = socket(AF_INET, SOCK_STREAM, 0);
+
+        struct in_addr in_addr;
+
+        if (inet_pton(AF_INET, "127.0.0.1", &in_addr) < 1)
+        {
+            perror("inet_pton failed");
+            return 1;
+        }
+
+        struct sockaddr_in addr =
+            {
+                AF_INET,
+                htons(8081),
+                in_addr};
+
+        if (connect(scon, (struct sockaddr *)&addr, sizeof(addr)) != 0) {
+            perror("connect failed");
+            return 1;
+        };
+
         uint8_t buf[1024] = {0};
         ssize_t used = 0;
         while (1)
         {
             ssize_t recvr = recv(cfd, buf + used, 1024 - used, 0);
-            used += recvr;
 
             if (recvr == -1)
             {
@@ -73,10 +100,29 @@ int main(int argc, char *argv[])
 
             if (recvr == 0 || request_is_complete(buf))
             {
+                uint8_t proxy_buf[4096] = {0};
+                size_t proxy_used = 0;
+
+                send(scon, buf + used, recvr, 0);
+
+                while (1)
+                {
+                    int proxy_recv_result = recv(scon, proxy_buf + proxy_used, sizeof(proxy_buf) - proxy_used, 0);
+                    if (proxy_recv_result == 0) {
+                        break;
+                    }
+                    send(cfd, proxy_buf + proxy_used, proxy_recv_result, 0);
+                    proxy_used += proxy_recv_result;
+                }
                 break;
             }
 
             printf("%s\n", (char *)buf);
+
+            send(scon, buf + used, recvr, 0);
+
+            used += recvr;
+
         }
 
         printf("Data has been received\n");
