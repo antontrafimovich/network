3ac4c6d92bb6678f6c88a93f08ae00dab3fe4ebb
Anton Trafimovich <tonytrof@gmail.com>
2026-08-12 12:32:59 +0200

    Updated dns-client.

--- dns-client/dns-query.c
+++ dns-client/dns-query.c
@@ -4,12 +4,99 @@
 #include <netinet/in.h>
 #include <stdio.h>
 #include <stdint.h>
+#include <string.h>
+
+struct domain_response
+{
+    char *name;
+    uint8_t addr;
+};
 
 void print_byte_binary(unsigned char byte)
 {
     for (int i = 7; i >= 0; i--)
     {
         printf("%d", (byte >> i) & 1);
+    }
+}
+
+void str_to_header_value(char *str, char *buf)
+{
+    unsigned char *label_len = (unsigned char *)buf++;
+    unsigned char len = 0;
+
+    while (*str != '\0')
+    {
+        if (*str == '.')
+        {
+            *label_len = len;
+            label_len = (unsigned char *)buf++;
+            len = 0;
+        }
+        else
+        {
+            *buf++ = *str;
+            len++;
+        }
+
+        str++;
+    }
+
+    *label_len = len;
+    *buf = 0;
+}
+
+int dns_response_to_user_frinedly(uint8_t *resp, size_t resp_length)
+{
+    int i;
+    unsigned int HEADER_LENGTH = 12;
+    unsigned short ancount = resp[7];
+
+    char name[32] = {0};
+    char *name_p = name;
+    size_t handled = HEADER_LENGTH;
+
+    resp += HEADER_LENGTH;
+
+    while (*resp != 0)
+    {
+        if (*resp < 40 || *resp > 122)
+        {
+            *name_p++ = '.';
+            handled++;
+            resp++;
+            continue;
+        }
+
+        *name_p++ = *resp++;
+        handled++;
+    }
+
+    *name_p++ = '\0';
+
+    resp += 5;
+    handled += 5;
+
+    int ip_octets_to_handle = 0;
+
+    while (handled < resp_length)
+    {
+        if (*resp == 0xc0)
+        {
+            printf("%s: ", name);
+            resp += 12;
+            ip_octets_to_handle = *(resp - 1);
+            handled += 12;
+        }
+
+        if (ip_octets_to_handle != 0)
+        {
+            while (ip_octets_to_handle-- > 0)
+            {
+                printf(ip_octets_to_handle == 0 ? "%d\n" : "%d.", *(unsigned char *)(resp++));
+                handled++;
+            }
+        }
     }
 }
 
@@ -36,6 +123,9 @@ int main(int argc, char **argv)
     }
 
     char buf[4096] = {0};
+    char *host = "www.example.com";
+    size_t question_len = strlen(host) + 2;
+
     buf[0] = 0x1f;
     buf[1] = 0x2f;
     buf[2] = 0;
@@ -48,29 +138,15 @@ int main(int argc, char **argv)
     buf[9] = 0;
     buf[10] = 0;
     buf[11] = 0;
-    buf[12] = 0x03;
-    buf[13] = 'w';
-    buf[14] = 'w';
-    buf[15] = 'w';
-    buf[16] = 0x07;
-    buf[17] = 'e';
-    buf[18] = 'x';
-    buf[19] = 'a';
-    buf[20] = 'm';
-    buf[21] = 'p';
-    buf[22] = 'l';
-    buf[23] = 'e';
-    buf[24] = 0x03;
-    buf[25] = 'c';
-    buf[26] = 'o';
-    buf[27] = 'm';
-    buf[28] = 0x0;
-    buf[29] = 0x0;
-    buf[30] = 'A';
-    buf[31] = 'I';
-    buf[32] = 'N';
 
-    if (send(fd, buf, 33, 0) == -1)
+    str_to_header_value(host, buf + 12);
+
+    buf[12 + question_len] = 0x0;
+    buf[13 + question_len] = 0x01;
+    buf[14 + question_len] = 0x0;
+    buf[15 + question_len] = 0x01;
+
+    if (send(fd, buf, 16 + question_len, 0) == -1)
     {
         perror("send failed");
         return 1;
@@ -101,12 +177,14 @@ int main(int argc, char **argv)
 
     int i;
     printf("The received dns result is: ");
-    for (i = 12; i < query_length; i++)
+    for (i = 0; i < query_length; i++)
     {
-        printf("\\");
-        print_byte_binary(query[i]);
+        printf("\\%02x", query[i]);
+        // print_byte_binary(query[i]);
     }
     printf("\n");
+
+    dns_response_to_user_frinedly(query, query_length);
 
     return 0;
 }
