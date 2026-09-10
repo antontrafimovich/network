6ca0db152bfa34ca5b45bc4610e836ccedf8795c
Anton Trafimovich <tonytrof@gmail.com>
2026-09-10 11:59:17 +0000

    Continue with dns resolver common functionality.

--- dns-compression/dns-query.c
+++ dns-compression/dns-query.c
@@ -5,6 +5,8 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <string.h>
+#include <stdbool.h>
+#include <stdlib.h>
 
 #define ISCOMPRESSED(b) ((b >> 6) == 0x03)
 
@@ -16,6 +18,31 @@ struct domain_response
 {
     char *name;
     uint8_t addr;
+};
+
+struct domain_message_question
+{
+    char name[256];
+    uint16_t type;
+    uint16_t class;
+};
+
+struct domain_message
+{
+    int16_t id;
+    bool qr;
+    uint8_t opcode;
+    bool aa_authoritative_answer;
+    bool tc_truncation;
+    bool rd_recursion_desired;
+    bool ra_recursion_available;
+    int8_t z_for_future_use;
+    uint8_t rcode_response_code;
+    uint16_t qdcount_question_section_entries_number;
+    uint16_t ancount_answer_section_entries_number;
+    uint16_t nscount_authority_section_entries_number;
+    uint16_t arcount_additinal_section_entries_number;
+    struct domain_message_question *questions;
 };
 
 void print_byte_binary(unsigned char byte)
@@ -52,62 +79,152 @@ void str_to_qname_value(char *str, char *buf)
     *buf = 0;
 }
 
-int dns_response_to_user_friendly(uint8_t *dns_response, size_t resp_length)
+int dns_name_to_string(uint8_t *dns_section_record_start, char *name)
 {
-    int i;
-    uint16_t ancount = dns_response[7];
-    uint8_t *dns_response_p = dns_response;
+    uint8_t *dns_section_record_start_p = dns_section_record_start;
 
-    char name[32] = {0};
-    char *name_p = name;
-    size_t handled = HEADER_SIZE;
-
-    dns_response_p += HEADER_SIZE;
-
-    while (*dns_response_p != 0)
+    while (*dns_section_record_start_p != 0)
     {
-        if (ISALPHANUMERIC(*dns_response_p))
+        if (ISALPHANUMERIC(*dns_section_record_start_p))
         {
-            *name_p++ = *dns_response_p++;
+            *name++ = *dns_section_record_start_p++;
         }
         else
         {
-            *name_p++ = '.';
-            dns_response_p++;
+            *name++ = '.';
+            dns_section_record_start_p++;
         }
-
-        handled++;
     }
 
-    *name_p++ = '\0';
+    *name = '\0';
 
-    dns_response_p += 5;
-    handled += 5;
+    return 0;
+}
 
-    int ip_octets_to_handle = 0;
+int parse_dns_question_entry(struct domain_message_question question, uint8_t *dns_response)
+{
+    const size_t null_root_label_length = 1;
+    uint8_t *dns_question_section_p = dns_response + 12;
 
-    printf("dns response byte: %d\n", *dns_response_p);
-    printf("is compressed: %d\n", ISCOMPRESSED(*dns_response_p));
-
-    while (handled < resp_length)
+    if (!ISCOMPRESSED(*dns_question_section_p))
     {
-        if (ISCOMPRESSED(*dns_response_p))
-        {
-            printf("%s: ", name);
-            dns_response_p += 12;
-            ip_octets_to_handle = *(dns_response_p - 1);
-            handled += 12;
-        }
-
-        if (ip_octets_to_handle != 0)
-        {
-            while (ip_octets_to_handle-- > 0)
-            {
-                printf(ip_octets_to_handle == 0 ? "%d\n" : "%d.", *(unsigned char *)(dns_response_p++));
-                handled++;
-            }
-        }
+        dns_name_to_string(dns_question_section_p, question.name);
+        dns_question_section_p += strlen(question.name) + null_root_label_length;
     }
+
+    // while (*dns_response_p != 0)
+    // {
+    //     if (ISCOMPRESSED(*dns_response_p))
+    //     {
+    //     }
+    //     else
+    //     {
+    //     }
+    // }
+}
+
+int dns_response_to_user_friendly(uint8_t *dns_response, size_t resp_length)
+{
+    struct domain_message domain_message_answer = {0};
+
+    uint8_t *dns_response_p = dns_response;
+
+    // start parse query response header
+    memcpy(&domain_message_answer.id, dns_response_p, 2);
+    domain_message_answer.id = ntohs(domain_message_answer.id);
+
+    dns_response_p += 2;
+
+    uint16_t codes = 0;
+    memcpy(&codes, dns_response_p, 2);
+    codes = ntohs(codes);
+    domain_message_answer.qr = (codes >> 15) & 1;
+    domain_message_answer.opcode = (codes >> 11) & 0x0f;
+    domain_message_answer.aa_authoritative_answer = (codes >> 10) & 1;
+    domain_message_answer.tc_truncation = (codes >> 9) & 1;
+    domain_message_answer.rd_recursion_desired = (codes >> 8) & 1;
+    domain_message_answer.ra_recursion_available = (codes >> 7) & 1;
+    domain_message_answer.z_for_future_use = (codes >> 4) & 7;
+    domain_message_answer.rcode_response_code = codes & 0x0f;
+
+    dns_response_p += 2;
+
+    domain_message_answer.qdcount_question_section_entries_number = (uint16_t)(dns_response_p[0] | dns_response_p[1]);
+    dns_response_p += 2;
+
+    domain_message_answer.ancount_answer_section_entries_number = (uint16_t)(dns_response_p[0] | dns_response_p[1]);
+    dns_response_p += 2;
+
+    domain_message_answer.nscount_authority_section_entries_number = (uint16_t)(dns_response_p[0] | dns_response_p[1]);
+    dns_response_p += 2;
+
+    domain_message_answer.arcount_additinal_section_entries_number = (uint16_t)(dns_response_p[0] | dns_response_p[1]);
+    dns_response_p += 2;
+    // end parse query response header
+
+    // parse question section entries
+    domain_message_answer.questions = malloc(sizeof(struct domain_message_question) * domain_message_answer.qdcount_question_section_entries_number);
+
+    size_t i = 0;
+    while (i < domain_message_answer.qdcount_question_section_entries_number)
+    {
+        parse_dns_question_entry(domain_message_answer.questions[i], dns_response_p);
+
+        i++;
+    }
+
+    // parse answer section entries
+    // int i;
+    // uint16_t ancount = dns_response[7];
+    // uint8_t *dns_response_p = dns_response;
+
+    // char name[32] = {0};
+    // char *name_p = name;
+    // size_t handled = HEADER_SIZE;
+
+    // dns_response_p += HEADER_SIZE;
+
+    // while (*dns_response_p != 0)
+    // {
+    //     if (ISALPHANUMERIC(*dns_response_p))
+    //     {
+    //         *name_p++ = *dns_response_p++;
+    //     }
+    //     else
+    //     {
+    //         *name_p++ = '.';
+    //         dns_response_p++;
+    //     }
+
+    //     handled++;
+    // }
+
+    // *name_p++ = '\0';
+
+    // dns_response_p += 5;
+    // handled += 5;
+
+    // int ip_octets_to_handle = 0;
+
+    // while (handled < resp_length)
+    // {
+    //     if (ISCOMPRESSED(*dns_response_p))
+    //     {
+    //         printf("%s: ", name);
+    //         dns_response_p += 12;
+    //         ip_octets_to_handle = *(dns_response_p - 1);
+    //         handled += 12;
+    //     }
+
+    //     if (ip_octets_to_handle != 0)
+    //     {
+    //         while (ip_octets_to_handle-- > 0)
+    //         {
+    //             printf(ip_octets_to_handle == 0 ? "%d\n" : "%d.", *(unsigned char *)(dns_response_p++));
+    //             handled++;
+    //         }
+    //     }
+    // }
 }
 
 int main(int argc, char **argv)
@@ -142,7 +259,7 @@ int main(int argc, char **argv)
 
     buf[0] = 0x1f;
     buf[1] = 0x2f;
-    buf[2] = 0;
+    buf[2] = 0x01;
     buf[3] = 0;
     buf[4] = 0;
     buf[5] = 1;
